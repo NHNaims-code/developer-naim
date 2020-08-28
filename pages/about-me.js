6a8297361beb89ccb3289775409cbab7f124fd60
NHNaims-code <mdnimurhasan99@gmail.com>
2020-08-28 21:58:29 +0600

    about me page added

--- pages/about-me.js
+++ pages/about-me.js
@@ -1,13 +1,13 @@
-document.getElementById('menu').addEventListener('click', function (){
+document.getElementById('menu-me').addEventListener('click', function (){
     console.log('clicked');
-    document.getElementById('body').innerHTML = `
+    document.getElementById('body-me').innerHTML = `
         <div class = "popup-menu">
             <div id = "close">
                 <a href=""><i class="fa fa-times" aria-hidden="true"></i></a>
             </div>
             <ul>
-                <li><a href=""><i class="fa fa-home" aria-hidden="true"></i>HOME</a></li>
-                <li><a href="pages/about-me.html"><i class="fa fa-user" aria-hidden="true"></i> ABOUT</a></li>
+                <li><a href="../index.html"><i class="fa fa-home" aria-hidden="true"></i>HOME</a></li>
+                <li><a href=""><i class="fa fa-user" aria-hidden="true"></i> ABOUT</a></li>
                 <li><a href=""><i class="fa fa-briefcase" aria-hidden="true"></i>PORTFOLIO</a></li>
                 <li><a href=""><i class="fa fa-envelope-open" aria-hidden="true"></i>CONTACT</a></li>
                 <li><a href=""><i class="fa fa-comments" aria-hidden="true"></i>BLOG</a></li>
@@ -15,6 +15,8 @@ document.getElementById('menu').addEventListener('click', function (){
         </div>
         `
     })
+
+
 
 console.log('clicked');
 
